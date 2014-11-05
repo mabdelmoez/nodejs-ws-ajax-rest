88f4bffcabbc1dda39eda15a838656c6c9d9f7f8
Mosti <Mosti@Mosti-PC>
2014-11-05 05:23:41 +0100

    views, ejs, ajax & ws

--- app.js
+++ app.js
@@ -2,45 +2,58 @@ var WebSocketServer = require("ws").Server;
 var http = require("http");
 var express = require("express");
 var bodyParser = require('body-parser');
+var path = require ('path'); //for windows enviroment 
 var port = 5000;
 
 var app = express();
+var router = express.Router(); 
 
-
- 
-    app.use(express.static(__dirname));
-	/*app.set('view engine', 'ejs');
-    app.get('/', function(req, res, next) {
-		 res.render('index');
-		 console.log('receiving index routing');
-    });*/
+ 	
+	app.set('view engine', 'ejs');
+	app.set('views', __dirname + '/public/views');
+	app.use(express.static(path.join(__dirname + '/public'))); //to work with link rel
+	//app.use(express.static(__dirname + '/public'));
 	
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
+	
+    app.get('/', function(req, res, next) {
+		 res.render('pages/index');
+    });
+	
+   app.get('/appajax', function(req, res) {
+    	res.render('pages/appajax', {
+    		username: 'user'
+    	})
+    });
 
-	var router = express.Router(); 
- 
-	
-	
-	app.get('/messaging', function(req, res, next) {
-		 console.log('receiving get request');
+    app.get('/appws', function(req, res) {
+    	res.render('pages/appws', {
+    		username: 'user'
+    	})
+    });
+
+    /*app.use('/', router);*/
+		
+	app.get('/api/messaging', function(req, res, next) {
+		 console.log('received api get request');
 		 console.log(req.body);
 		 res.json(req.body);
     });
     
-	app.post('/messaging', function(req, res, next) {
-       console.log('receiving post request');
+	app.post('/api/messaging', function(req, res, next) {
+       console.log('receiving api post request');
 	   res.send(req.body);
     });
 	
 	
-	app.use('/messaging', router);
+	app.use('/api/messaging', router);
     
 
 	app.server = http.createServer(app);
     app.server.listen(port);
 
-console.log("http server listening on %d", port);
+    console.log("app server listening on %d", port);
 
 var userId;
 var wss = new WebSocketServer({server: app.server});
@@ -56,14 +69,11 @@ var wss = new WebSocketServer({server: app.server});
     ws.on("message", function (data, flags) {
         console.log("websocket received a message");
         var clientMsg = data;
-
+        console.log(clientMsg);
         ws.send(JSON.stringify({msg:{connectionId:userId, Data:clientMsg}}));
-
-
     });
 
     ws.on("close", function () {
         console.log("websocket connection close");
     });
 });
-console.log("websocket server created");
